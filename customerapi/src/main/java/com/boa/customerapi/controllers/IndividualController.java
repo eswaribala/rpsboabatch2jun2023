f17fd909222ee5d97c1954a2f0acb318e899db2e
eswaribala <parameswaribala@gmail.com>
2023-06-12 15:59:28 +0530

    updated

--- customerapi/src/main/java/com/boa/customerapi/controllers/IndividualController.java
+++ customerapi/src/main/java/com/boa/customerapi/controllers/IndividualController.java
@@ -33,6 +33,45 @@ public class IndividualController {
    public List<Individual> getAllIndividuals(){
        return this.individualService.findAllIndividuals();
    }
+    @GetMapping("/v1.0/{accountNo}")
+    public ResponseEntity<ResponseWrapper> getIndividualById(@PathVariable("accountNo") long accountNo){
+        Individual indobj= this.individualService.findIndividualById(accountNo);
+        if(indobj!=null){
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(new ResponseWrapper(indobj));
+        }
+        else
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseWrapper("Individual not available"));
+    }
+
+    @GetMapping("/v1.0/contactno/{contactNo}")
+    public ResponseEntity<ResponseWrapper> getIndividualByContactNo(@PathVariable("contactNo") long contactNo){
+        List<Individual> individuals= this.individualService.findIndividualByContactNo(contactNo);
+        if(individuals.size()>0){
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(new ResponseWrapper(individuals));
+        }
+        else
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseWrapper("Individual not available"));
+    }
 
 
+    @PutMapping("/v1.0/{accountNo}")
+    public ResponseEntity<ResponseWrapper> updateIndividualEmailById(@PathVariable("accountNo") long accountNo,
+                                                                     @RequestParam("email") String email){
+        Individual indobj= this.individualService.updateIndividualEmail(accountNo,email);
+        if(indobj!=null){
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(new ResponseWrapper(indobj));
+        }
+        else
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseWrapper("Individual not updated"));
+    }
+    @DeleteMapping("/v1.0/{accountNo}")
+    public ResponseEntity<ResponseWrapper> deleteIndividualById(@PathVariable("accountNo") long accountNo){
+
+
+        if(this.individualService.deleteIndividualById(accountNo)){
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(new ResponseWrapper("Individual Deleted"));
+        }
+        else
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseWrapper("Individual not deleted"));
+    }
 }
