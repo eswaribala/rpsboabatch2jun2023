f9ee35277a9e6f2a03192399bdc6ef5f7b4cbd50
eswaribala <parameswaribala@gmail.com>
2023-06-16 14:33:26 +0530

    updated

--- accountapisubscriber/src/main/java/com/boa/accountapisubscriber/services/CustomerConsumerService.java
+++ accountapisubscriber/src/main/java/com/boa/accountapisubscriber/services/CustomerConsumerService.java
@@ -2,6 +2,8 @@ package com.boa.accountapisubscriber.services;
 
 import com.boa.accountapisubscriber.dtos.Individual;
 import com.boa.accountapisubscriber.facades.CustomerFacade;
+import com.boa.accountapisubscriber.repositories.IndividualRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
@@ -9,7 +11,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomerConsumerService {
 
-
+    @Autowired
+   private IndividualRepo individualRepo;
 
 
     @StreamListener(target = CustomerFacade.inChannelName)
@@ -17,6 +20,7 @@ public class CustomerConsumerService {
 
         System.out.println("Received Individual details: "+individual);
 
+        this.individualRepo.save(individual);
     }
 
 }
